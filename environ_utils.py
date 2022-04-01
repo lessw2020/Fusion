4e2688b07915ef9d67cf4eb9ef45aac488055e77
lessw2020 <lessw@etrillium.com>
2022-04-01 22:05:30 +0000

    fix rank checking

--- environ_utils.py
+++ environ_utils.py
@@ -3,38 +3,6 @@ import torch
 import psutil
 
 
-def set_singleton_view_gpu(local_rank: int):
-    os.environ["CUDA_VISIBLE_DEVICES"] = f"{local_rank}"
-
-
-def is_rank_0():
-    return 0 == int(os.getenv("RANK"))
-
-
-def get_rank():
-    return int(os.getenv("RANK"))
-
-
-def get_world_size():
-    return int(os.getenv("WORLD_SIZE"))
-
-
-def get_local_rank():
-    """Get local rank - this is sometimes None"""
-    return int(os.getenv("LOCAL_RANK"))
-
-
-# memory
-def print_memory_summary(prefix, device):
-    if is_rank_0:
-        print(
-            f"{prefix}, GPU memory allocation: {torch.cuda.max_memory_allocated(device) // 1e9}GB "
-            f"CPU used memory percent: {psutil.virtual_memory().percent}, "
-            f"CPU memory available: {psutil.virtual_memory().available // 1e9}GB, "
-        )
-        torch.cuda.reset_peak_memory_stats(device)
-
-
 def calc_flop(cfg):
     """todo: this pulls layer, model and vocab size to calc flops"""
     B = args.batch_size
