c1a4b23271ae669687a184092abb14bd0b58fa8c
lessw2020 <lessw@etrillium.com>
2022-04-05 02:18:19 +0000

    start using dataclass for cfg, start wikihow integration

--- dataset_builder.py
+++ dataset_builder.py
@@ -9,27 +9,39 @@ from torchvision import datasets, transforms
 from torch.utils.data.distributed import DistributedSampler
 
 
-def build_train_transforms():
+def build_train_transforms(cfg=None):
+    """build transform pipeline for training"""
+    train_transform = None
+    print(f"Training transform = {cfg.train_transform}")
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-    )
+    if cfg.train_transform == "mnist_train":
+        train_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+    else:
+        print("uknown train transform!!")
 
-    return transform
+    return train_transform
 
 
-def build_val_transforms():
+def build_val_transforms(cfg=None):
     """transforms for validatin and prediction"""
-    if 0 == int(os.getenv("RANK")):
-        print(f" --> using training transforms")
+    val_transform = None
 
-    return build_train_transforms()  # todo - this is only for mnist example
+    if cfg.val_transform == "mnist_val":
+        val_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+    else:
+        print(f"Unknown validation transform!")
+
+    return val_transform
 
 
-def build_training_dataloader(batch_size=4):
+def build_training_dataloader(cfg=None):
     """build out the settings and return training dataloader"""
 
-    train_transform = build_train_transforms()
+    train_transform = build_train_transforms(cfg)
 
     dataset_training = datasets.MNIST(
         "./data", train=True, download=True, transform=train_transform
@@ -42,7 +54,7 @@ def build_training_dataloader(batch_size=4):
         dataset_training, rank=rank, num_replicas=world_size, shuffle=True
     )
 
-    train_kwargs = {"batch_size": batch_size, "sampler": sampler_training}
+    train_kwargs = {"batch_size": cfg.batch_size, "sampler": sampler_training}
 
     cuda_kwargs = {"num_workers": 2, "pin_memory": True, "shuffle": False}
     train_kwargs.update(cuda_kwargs)
